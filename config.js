911169132ecc62cf9b0a82175aab3cc1c5c4811c
David Cyril <168988025+DeeCeeXxx@users.noreply.github.com>
2024-12-26 16:18:13 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -1,23 +1,21 @@
-global.owner = [
-  "2347043759577",
-  ""
-]
-global.anitav4 = "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒"
-global.ownername = "David Cyril"
-global.packname = "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒"
-global.author = "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋"
-global.channelname = "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋"
-global.channeljid = "120363315231436175@newsletter"
-global.autoTyping = false;                //make true to enable auto typing
-global.autoRecord = false;                //make true to enable auto recording
-global.autoViewStatus = true;   // Set to true to automatically read statuses
-global.AUTOSTATUS_REACT = false;   // Set to true to automatically like statuses
-global.autoLikeEmoji = "💚";    // Default emoji for liking statuses
+require('dotenv').config(); 
 
-global.LEVELUP = false;
+global.owner = process.env.OWNER.split(','); 
+global.anitav4 = process.env.BOT_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
+global.ownername = process.env.OWNER_NAME || "David Cyril";
+global.packname = process.env.PACK_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
+global.author = process.env.AUTHOR || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
+global.channelname = process.env.CHANNEL_NAME || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
+global.channeljid = process.env.CHANNEL_JID || "120363315231436175@newsletter";
+global.autoTyping = process.env.AUTO_TYPING === 'true'; 
+global.autoRecord = process.env.AUTO_RECORD === 'true';
+global.autoViewStatus = process.env.AUTO_VIEW_STATUS === 'true';
+global.AUTOSTATUS_REACT = process.env.AUTOSTATUS_REACT === 'true';
+global.autoLikeEmoji = process.env.AUTO_LIKE_EMOJI || "💚";
 
+global.LEVELUP = process.env.LEVELUP === 'true';
 
-global.ANTIVIEWONCE = false;
+global.ANTIVIEWONCE = process.env.ANTIVIEWONCE === 'true';
 
 global.roles = {
     1: "Novice",
@@ -30,50 +28,42 @@ global.roles = {
     25: "Legend",
 };
 
-global.sudoUsers = ["2349066528353@s.whatsapp.net", "2348129988915@s.whatsapp.net"]; // Add the JIDs of sudo users
+global.sudoUsers = process.env.SUDO_USERS.split(',').map(user => `${user}@s.whatsapp.net`); 
 
+global.public = process.env.PUBLIC === 'true';
 
+global.ANTIDELETE = process.env.ANTIDELETE === 'true';
+global.unavailable = process.env.UNAVAILABLE === 'true';
+global.available = process.env.AVAILABLE === 'true';
+global.autoreadmessages = process.env.AUTO_READ_MESSAGES === 'true';
+global.chatbot = process.env.CHATBOT === 'true';
+global.autoreact = process.env.AUTO_REACT === 'true';
 
-global.public = true
+global.WELCOME = process.env.WELCOME === 'true';
 
+global.prefix = process.env.PREFIX || '.';
 
-global.ANTIDELETE = false;  
+global.autobio = process.env.AUTO_BIO === 'true';
 
-global.unavailable = true;     
-//to show your real presence  
-global.available = false;   
-// to show always online  
-global.autoreadmessages = false;   
-///To always read messages
-global.chatbot = false;
-global.autoreact = false;
+global.ANTICALL = process.env.ANTICALL === 'true';
 
-global.WELCOME = false;
-
-global.prefix = '.';
-
-global.autobio = false;
-
-global.ANTICALL = false;
-
-global.antilink = false;
-
-global.antilinkkick = false;
-
-global.antilinkwarn = false;
+global.antilink = process.env.ANTILINK === 'true';
+global.antilinkkick = process.env.ANTILINK_KICK === 'true';
+global.antilinkwarn = process.env.ANTILINK_WARN === 'true';
 
 global.mess = {
-    success: '𝙳𝚘𝚗𝚎',
-    admin: '_*❗This Command Can Only Be Used By Group Admins !*_',
-    botAdmin: '_*❗This Command Can Only Be Used When Bot Becomes Group Admin !*_',
-    OnlyOwner: '_*❗This Command Can Only Be Used By My Owner !*_',
-    OnlyGrup: '_*❗This Command Can Only Be Used In Group Chat !*_',
-    private: '_(❗This Command Can Only Be Used In Private Chat !*_',
-    wait: '_*Please Wait*_',
-    notregist: '_*You are not registered in the Bot Database. Please register first_*',
-    premium: '_*Premium only" Want Premium? Chat Owner_*',
-    endLimit: '_*Your Daily Limit Has Been Used Up, The Limit Will Be Reset Every 00:00 AM_*.',
-}
+    success: process.env.MESSAGE_SUCCESS || '𝙳𝚘𝚗𝚎',
+    admin: process.env.MESSAGE_ADMIN || '_*❗This Command Can Only Be Used By Group Admins !*_',
+    botAdmin: process.env.MESSAGE_BOT_ADMIN || '_*❗This Command Can Only Be Used When Bot Becomes Group Admin !*_',
+    OnlyOwner: process.env.MESSAGE_OWNER || '_*❗This Command Can Only Be Used By My Owner !*_',
+    OnlyGrup: process.env.MESSAGE_GROUP || '_*❗This Command Can Only Be Used In Group Chat !*_',
+    private: process.env.MESSAGE_PRIVATE || '_*❗This Command Can Only Be Used In Private Chat !*_',
+    wait: process.env.MESSAGE_WAIT || '_*Please Wait*_',
+    notregist: process.env.MESSAGE_NOT_REGIST || '_*You are not registered in the Bot Database. Please register first*_',
+    premium: process.env.MESSAGE_PREMIUM || '_*Premium only" Want Premium? Chat Owner*_',
+    endLimit: process.env.MESSAGE_END_LIMIT || '_*Your Daily Limit Has Been Used Up, The Limit Will Be Reset Every 00:00 AM_*.',
+};
+
 
 let fs = require('fs')
 let file = require.resolve(__filename)
